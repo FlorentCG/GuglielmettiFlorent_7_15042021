1759236edeeecdd753d6226224c6dfd52b0e725c
Florent Guglielmetti <florent.guglielmetti@gmail.com>
2021-05-05 15:15:32 +0200

    Modifs routes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,10 +2,10 @@ const express= require ('express'); // Utilisation framework basé sur node.JS /
 const router= express.Router(); 
 const userCtrl= require ('../controllers/user');
 const auth = require ('../middleware/auth');
-const expressBouncer= require ("express-bouncer")(5000,600000,3); // Contre les attaques de force Brute // 
+const expressBouncer= require ("express-bouncer")(5000,600000,3);
 
 router.post('/signup', userCtrl.signup);
-router.post('/login',expressBouncer.block, userCtrl.login);
+router.post('/login', expressBouncer.block, userCtrl.login);
 router.get('/users/:id',auth,userCtrl.getUserProfile);
 router.put('/users/:id',auth, userCtrl.updatePwd);
 router.delete('/users/:id',auth,userCtrl.delete);
